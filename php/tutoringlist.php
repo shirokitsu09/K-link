927fabad12fae4ac77900e201927a216f69d81b0
Not6629 <154392788+Not6629@users.noreply.github.com>
2023-12-27 01:26:43 +0700

    Add files via upload

---
+++ php/tutoringlist.php
@@ -0,0 +1,373 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="initial-scale=1, width=device-width" />
+    <link rel="stylesheet" href="./css/global.css" />
+    <link rel="stylesheet" href="./css/tutoringlist.css" />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+    />
+  </head>
+  <body>
+    <div class="list-frame">
+      <div class="list-1" id="list-1">
+        <div class="list-inner" id="innerlist-1">
+        <div class="tag1">วิศวกรรมศาสตร์</div>
+        <div class="tag2">Calculus</div>
+        <div class="tag3">วิศวกรรมคอมพิวเตอร์</div>
+        <b class="group-amount">2/5</b>
+        <b class="subject">แคลเซคจารย์สยาม</b>
+        <div class="mentor">ผู้สอน : AIJHONG</div>
+        <div class="group-description">รายละเอียด : polar coordinates</div>
+        <div class="group-date">เวลา : 15 มิ.ย. 2567 17.00 - 19.00</div>
+        <div class="group-location">สถานที่ : โต๊ะรูม A (โดมเห็ดข้างโรง A)</div>
+        <b class="faculty">วิศวกรรมศาสตร์</b>
+        </div>
+        <img class="flag-banner" alt="" src="./public/tutoringbanner.svg" />
+        <img class="Tdot-button" alt="" src="./public/threedot.svg" />
+        <div class="tutoringjoin" id="tutoringjoin-1" style="display: none;">
+        <div class="join-button" id="join-1" value="join-1">
+            <div class="group">
+              <div class="button-text">เข้าร่วมกลุ่ม</div>
+              <img class="button-icon" alt="" src="./public/tutoring-join.svg" />
+            </div>
+          </div>
+
+          <div class="member-button" id="member-1">
+            <div class="group">
+              <div class="button-text">สมาชิกกลุ่ม</div>
+              <img class="button-icon" alt="" src="./public/tutoring-member.svg" />
+            </div>
+          </div>
+
+          <div class="close-button" id="close-1">
+                <div class="group">
+                  <div class="button-text">ปิด</div>
+                  <img class="button-icon1" alt="" src="./public/tutoring-close.svg" />
+                </div>
+          </div>
+      </div>
+      </div>
+
+      <div class="list-2" id="list-2">
+        <div class="list-inner" id="innerlist-2">
+        <div class="tag1">วิทยาศาสตร์</div>
+        <div class="tag2">Biology</div>
+        <div class="tag3">ชีวะนาโน</div>
+            <b class="group-amount">3/3</b>
+            <b class="subject">ชีวะชีใจ</b>
+            <div class="mentor">ผู้สอน : เจ้าชายไอโบ</div>
+            <div class="group-description">รายละเอียด : สอบปลายภาค</div>
+            <div class="group-date">เวลา : 1 ม.ค. 2555 13.30 - 16.30</div>
+            <div class="group-location">สถานที่ : พระจอมเกล้า 908</div>
+            <b class="faculty">วิทยาศาสตร์</b>
+        </div>
+        <img class="flag-banner" alt="" src="./public/tutoringbanner.svg" />
+        <img class="Tdot-button" alt="" src="./public/threedot.svg" />
+        <div class="tutoringjoin" id="tutoringjoin-2" style="display: none;">
+          <div class="join-button" id="join-2">
+            <div class="group">
+              <div class="button-text">เข้าร่วมกลุ่ม</div>
+              <img class="button-icon" alt="" src="./public/tutoring-join.svg" />
+            </div>
+          </div>
+
+          <div class="member-button" id="member-2">
+            <div class="group">
+              <div class="button-text">สมาชิกกลุ่ม</div>
+              <img class="button-icon" alt="" src="./public/tutoring-member.svg" />
+            </div>
+          </div>
+
+          <div class="close-button" id="close-2">
+                <div class="group">
+                  <div class="button-text">ปิด</div>
+                  <img class="button-icon1" alt="" src="./public/tutoring-close.svg" />
+                </div>
+          </div>
+      </div>
+      </div>
+
+      <div class="list-3" id="list-3">
+        <div class="list-inner" id="innerlist-3">
+        <div class="tag1">วิศวกรรมศาสตร์</div>
+        <div class="tag2">Calculus</div>
+        <div class="tag3"></div>
+            <b class="group-amount">9/ไม่จำกัด</b>
+            <b class="subject">แคล 2</b>
+            <div class="mentor">ผู้สอน : เหน่ง iot</div>
+            <div class="group-description">รายละเอียด : polar coordinates</div>
+            <div class="group-date">เวลา : 15 มิ.ย. 2567 17.00 - 19.00</div>
+            <div class="group-location">สถานที่ : Google meet</div>
+            <b class="faculty">วิศวกรรมศาสตร์</b>
+        </div> 
+        <img class="flag-banner" alt="" src="./public/tutoringbanner.svg" />
+        <img class="Tdot-button" alt="" src="./public/threedot.svg" />
+        <div class="tutoringjoin" id="tutoringjoin-3" style="display: none;">
+        <div class="join-button" id="join-3">
+          <div class="group">
+            <div class="button-text">เข้าร่วมกลุ่ม</div>
+            <img class="button-icon" alt="" src="./public/tutoring-join.svg" />
+          </div>
+        </div>
+
+        <div class="member-button" id="member-3">
+          <div class="group">
+            <div class="button-text">สมาชิกกลุ่ม</div>
+            <img class="button-icon" alt="" src="./public/tutoring-member.svg" />
+          </div>
+        </div>
+
+        <div class="close-button" id="close-3">
+              <div class="group">
+                <div class="button-text">ปิด</div>
+                <img class="button-icon1" alt="" src="./public/tutoring-close.svg" />
+              </div>
+        </div>
+      </div>
+      
+      </div>
+
+      <div class="list-4" id="list-4">
+        <div class="list-inner" id="innerlist-4">
+        <div class="tag1">วิศวกรรมศาสตร์</div>
+        <div class="tag2">Circuits</div>
+        <div class="tag3">วิศวกรรมคอมพิวเตอร์</div>
+            <b class="group-amount">10/10</b>
+            <b class="subject">เซอกิตจารย์ตู๋</b>
+            <div class="mentor">ผู้สอน : โอมZA2595</div>
+            <div class="group-description">รายละเอียด : สอบปลายภาค</div>
+            <div class="group-date">เวลา : 5 ก.พ. 2558 12.30 - 00.00</div>
+            <div class="group-location">สถานที่ : E-12 1007</div>
+            <b class="faculty">วิศวกรรมศาสตร์</b>
+        </div>
+        <img class="flag-banner" alt="" src="./public/tutoringbanner.svg" />
+        <img class="Tdot-button" alt="" src="./public/threedot.svg" />
+        <div class="tutoringjoin" id="tutoringjoin-4" style="display: none;">
+        <div class="join-button" id="join-4">
+          <div class="group">
+            <div class="button-text">เข้าร่วมกลุ่ม</div>
+            <img class="button-icon" alt="" src="./public/tutoring-join.svg" />
+          </div>
+        </div>
+
+        <div class="member-button" id="member-4">
+          <div class="group">
+            <div class="button-text">สมาชิกกลุ่ม</div>
+            <img class="button-icon" alt="" src="./public/tutoring-member.svg" />
+          </div>
+        </div>
+
+        <div class="close-button" id="close-4">
+              <div class="group">
+                <div class="button-text">ปิด</div>
+                <img class="button-icon1" alt="" src="./public/tutoring-close.svg" />
+              </div>
+        </div>
+      </div>
+      </div>
+
+      <div class="list-5" id="list-5">
+        <div class="list-inner" id="innerlist-5">
+        <div class="tag1">วิทยาศาสตร์</div>
+        <div class="tag2">Electronic Circuits</div>
+        <div class="tag3">ฟิสิกส์อุตสาหกรรม</div>
+            <b class="group-amount">20/ไม่จำกัด</b>
+            <b class="subject">อิเล็กจารย์ส...</b>
+            <div class="mentor">ผู้สอน : ตัวตึงภาคฟิ</div>
+            <div class="group-description">รายละเอียด : Test 2 - 3</div>
+            <div class="group-date">เวลา : 18 ก.ย. 2568 00.30 - 05.30</div>
+            <div class="group-location">สถานที่ : โรงครุ</div>
+            <b class="faculty">วิทยาศาสตร์</b>
+        </div>
+          <img class="flag-banner" alt="" src="./public/tutoringbanner.svg" />
+          <img class="Tdot-button" alt="" src="./public/threedot.svg" />
+          <div class="tutoringjoin" id="tutoringjoin-5" style="display: none;">
+          <div class="join-button" id="join-5">
+          <div class="group">
+            <div class="button-text">เข้าร่วมกลุ่ม</div>
+            <img class="button-icon" alt="" src="./public/tutoring-join.svg" />
+          </div>
+        </div>
+
+        <div class="member-button" id="member-5">
+          <div class="group">
+            <div class="button-text">สมาชิกกลุ่ม</div>
+            <img class="button-icon" alt="" src="./public/tutoring-member.svg" />
+          </div>
+        </div>
+
+        <div class="close-button" id="close-5">
+              <div class="group">
+                <div class="button-text">ปิด</div>
+                <img class="button-icon1" alt="" src="./public/tutoring-close.svg" />
+              </div>
+        </div>
+      </div>
+      </div>
+    </div>
+
+
+    <script>
+      var display = 0;
+      var list1 = document.getElementById("list-1");
+      var tutoringjoin1 =document.getElementById("tutoringjoin-1");
+      var innerlist1 = document.getElementById("innerlist-1");
+
+      var list2 = document.getElementById("list-2");
+      var tutoringjoin2 =document.getElementById("tutoringjoin-2");
+      var innerlist2 = document.getElementById("innerlist-2");
+
+      var list3 = document.getElementById("list-3");
+      var tutoringjoin3 =document.getElementById("tutoringjoin-3");
+      var innerlist3 = document.getElementById("innerlist-3");
+
+      var list4 = document.getElementById("list-4");
+      var tutoringjoin4 =document.getElementById("tutoringjoin-4");
+      var innerlist4 = document.getElementById("innerlist-4");
+
+      var list5 = document.getElementById("list-5");
+      var tutoringjoin5 =document.getElementById("tutoringjoin-5");
+      var innerlist5 = document.getElementById("innerlist-5");
+      
+      function close_tutoringjoin(e){
+        tutoringjoin1.style.display = 'none';
+        tutoringjoin2.style.display = 'none';
+        tutoringjoin3.style.display = 'none';
+        tutoringjoin4.style.display = 'none';
+        tutoringjoin5.style.display = 'none';
+        document.querySelector('.padding')?.classList.remove('padding')
+      }
+
+      var close1 = document.getElementById("close-1");
+      close1.addEventListener("click", close_tutoringjoin);
+      var close2 = document.getElementById("close-2");
+      close2.addEventListener("click", close_tutoringjoin);
+      var close3 = document.getElementById("close-3");
+      close3.addEventListener("click", close_tutoringjoin);
+      var close4 = document.getElementById("close-4");
+      close4.addEventListener("click", close_tutoringjoin);
+      var close5 = document.getElementById("close-5");
+      close5.addEventListener("click", close_tutoringjoin);
+
+      var join1 = document.getElementById("join-1");
+      join1.addEventListener("click", function(e){
+        console.log("join group1 requested");
+        close_tutoringjoin();
+      });
+
+      var join2 = document.getElementById("join-2");
+      join2.addEventListener("click", function(e){
+        console.log("join group2 requested");
+        close_tutoringjoin();
+      });
+
+      var join3 = document.getElementById("join-3");
+      join3.addEventListener("click", function(e){
+        console.log("join group3 requested");
+        close_tutoringjoin();
+      });
+
+      var join4 = document.getElementById("join-4");
+      join4.addEventListener("click", function(e){
+        console.log("join group4 requested");
+        close_tutoringjoin();
+      });
+
+      var join5 = document.getElementById("join-5");
+      join5.addEventListener("click", function(e){
+        console.log("join group5 requested");
+        close_tutoringjoin();
+      });
+      
+   
+      var member1 = document.getElementById("member-1")
+      member1.addEventListener("click", function (e) {
+        console.log("member group1 view requested")
+        close_tutoringjoin();
+      });
+
+      var member2 = document.getElementById("member-2")
+      member2.addEventListener("click", function (e) {
+        console.log("member group2 view requested")
+        close_tutoringjoin();
+      });
+
+      var member3 = document.getElementById("member-3")
+      member3.addEventListener("click", function (e) {
+        console.log("member group3 view requested")
+        close_tutoringjoin();
+      });
+
+      var member4 = document.getElementById("member-4")
+      member4.addEventListener("click", function (e) {
+        console.log("member group4 view requested")
+        close_tutoringjoin();
+      });
+
+      var member5 = document.getElementById("member-5")
+      member5.addEventListener("click", function (e) {
+        console.log("member group5 view requested")
+        close_tutoringjoin();
+      });
+
+      if (innerlist1) {
+        innerlist1.addEventListener("click", function (e) {
+          if (list1 != 'list-1.padding'){
+            close_tutoringjoin();
+            tutoringjoin1.style.display = 'block';
+            list1.classList.add('padding');
+          }
+        });
+      }
+
+      if (innerlist2) {
+        innerlist2.addEventListener("click", function (e) {
+          if (list2 != 'list-2.padding'){
+            close_tutoringjoin();
+            tutoringjoin2.style.display = 'block';
+            document.querySelector('.padding')?.classList.remove('padding')
+            list2.classList.add('padding');
+          }
+        });
+      }
+
+      if (innerlist3) {
+        innerlist3.addEventListener("click", function (e) {
+          if (list3 != 'list-3.padding'){
+            close_tutoringjoin();
+            tutoringjoin3.style.display = 'block';
+            document.querySelector('.padding')?.classList.remove('padding')
+            list3.classList.add('padding');
+          }
+        });
+      }
+
+      if (innerlist4) {
+        innerlist4.addEventListener("click", function (e) {
+          if (list4 != 'list-4.padding'){
+            close_tutoringjoin();
+            tutoringjoin4.style.display = 'block';
+            document.querySelector('.padding')?.classList.remove('padding')
+            list4.classList.add('padding');
+          }
+        });
+      }
+      
+      if (innerlist5) {
+        innerlist5.addEventListener("click", function (e) {
+          if (list5 != 'list-5.padding'){
+            close_tutoringjoin();
+            tutoringjoin5.style.display = 'block';
+            document.querySelector('.padding')?.classList.remove('padding')
+            list5.classList.add('padding');
+          }
+        });
+      }
+      
+    </script>
+
+  </body>
+</html>
