8bc196d4ea12e424770123bd075bfe97bda954cd
Not6629 <154392788+Not6629@users.noreply.github.com>
2024-01-05 16:25:06 +0700

    Group list

---
+++ php/grouplist.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="initial-scale=1, width=device-width" />
+    <link rel="stylesheet" href="../css/global.css" />
+    <link rel="stylesheet" href="../css/grouplist.css" />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+    />
+  </head>
+  <body>
+    <div class="list-frame">
+      <div class="status-bar"></div>
+      <div class="list" id="list-1">
+        <div class="list-inner" id="innerlist-1">
+        <img class="flag-banner" alt="" src="../images/grouplist/groupbanner.svg" />
+        <img class="Tdot-button" alt="" src="../images/tutoringlist/threedot.svg" />
+            <div class="tag-group">
+            <div class="tag">กีฬาและการออกกำลังกาย</div>
+            <div class="tag">ชิลๆ</div>
+            </div>
+            <div class="tag-group1">
+            <div class="tag"></div>
+            <div class="tag"></div>
+            </div>
+        <b class="group-amount">1/10</b>
+        <b class="group-name">หาเพื่อนไปวิ่งกันเถอะ232...</b>
+        <div class="leader">AIJHONG</div>
+        <img class="group-profile-picture" alt="" src="../images/grouplist/group-profilepic1.svg" />
+        <div class="group-date">วัน : 15 มิ.ย. 2567</div>
+        <div class="group-time">เวลา : 17:00 - 19:00 น.</div>
+        <div class="group-location">สถานที่ : สนามกีฬา</div>
+        <div class="group-description">รายละเอียด : ผู้เล่นจะได้รับบทบาทเป็น โกะโจ เพื่อช่วยเหล่าผู้เล่นให้ได้มากที่สุด ขณะเดียวกันผู้เล่นคนอื่น ๆ </div>
+        </div>
+        
+        <div class="tutoringjoin" id="tutoringjoin-1" style="display: none;">
+        <div class="join-button" id="join-1" value="join-1">
+            <div class="group">
+              <div class="button-text">เข้าร่วมกลุ่ม</div>
+              <img class="button-icon" alt="" src="../images/tutoringlist/tutoring-join.svg" />
+            </div>
+          </div>
+
+          <div class="member-button" id="member-1">
+            <div class="group">
+              <div class="button-text">สมาชิกกลุ่ม</div>
+              <img class="button-icon" alt="" src="../images/tutoringlist/tutoring-member.svg" />
+            </div>
+          </div>
+
+          <div class="close-button" id="close-1">
+                <div class="group">
+                  <div class="button-text">ปิด</div>
+                  <img class="button-icon1" alt="" src="../images/tutoringlist/tutoring-close.svg" />
+                </div>
+          </div>
+      </div>
+      </div>
+
+      <div class="list" id="list-2">
+        <div class="list-inner" id="innerlist-2">
+        <img class="flag-banner" alt="" src="../images/grouplist/groupbanner.svg" />
+        <img class="Tdot-button" alt="" src="../images/tutoringlist/threedot.svg" />
+                <div class="tag-group">
+                <div class="tag">อาหารการกิน</div>
+                <div class="tag">ชาบูปิ้งย่าง</div>
+                <div class="tag">ซอยเกกี</div>
+                </div>
+                <div class="tag-group1">
+                <div class="tag"></div>
+                </div>
+            <b class="group-amount">4/4</b>
+            <b class="group-name">หมูกะทะหารสี่</b>
+            <div class="leader">ราชาหมูกะทะผู้ยิ่งใหญ่</div>
+            <img class="group-profile-picture" alt="" src="../images/grouplist/group-profilepic2.svg" />
+            <div class="group-description">รายละเอียด : มากินหมูกะทะกันหาร 4 คน</div>
+            <div class="group-date">วัน : 25 ก.ค. 2589</div>
+            <div class="group-time">เวลา : 16:00 - 19:00 น.</div>
+            <div class="group-location">สถานที่ : หมูกะทะเกกี4</div>
+        </div>
+        
+        <div class="tutoringjoin" id="tutoringjoin-2" style="display: none;">
+          <div class="join-button" id="join-2">
+            <div class="group">
+              <div class="button-text">เข้าร่วมกลุ่ม</div>
+              <img class="button-icon" alt="" src="../images/tutoringlist/tutoring-join.svg" />
+            </div>
+          </div>
+
+          <div class="member-button" id="member-2">
+            <div class="group">
+              <div class="button-text">สมาชิกกลุ่ม</div>
+              <img class="button-icon" alt="" src="../images/tutoringlist/tutoring-member.svg" />
+            </div>
+          </div>
+
+          <div class="close-button" id="close-2">
+                <div class="group">
+                  <div class="button-text">ปิด</div>
+                  <img class="button-icon1" alt="" src="../images/tutoringlist/tutoring-close.svg" />
+                </div>
+          </div>
+      </div>
+      </div>
+
+      <div class="list" id="list-3">
+        <div class="list-inner" id="innerlist-3">
+        <img class="flag-banner" alt="" src="../images/grouplist/groupbanner.svg" />
+        <img class="Tdot-button" alt="" src="../images/tutoringlist/threedot.svg" />
+            <div class="tag-group">
+            <div class="tag">วิศวกรรมศาสตร์</div>
+            <div class="tag">Calculus</div>
+            <div class="tag"></div>
+            </div>
+            <b class="group-amount">10/ไม่จำกัด</b>
+            <b class="group-name">เชียร์บาสภาคคอมรวมตัว</b>
+            <div class="leader">เหน่ง iot</div>
+            <img class="group-profile-picture" alt="" src="../images/grouplist/group-profilepic3.svg" />
+            <div class="group-description">รายละเอียด : เพื่อช่วยเหล่าผู้เล่นให้ได้มากที่สุด ขณะเดียวกันผู้เล่นคนอื่น ๆ </div>
+            <div class="group-date">วัน : 9 ธ.ค. 2565</div>
+            <div class="group-time">เวลา : 17:00 - 21:00 น.</div>
+            <div class="group-location">สถานที่ : สนามบาสข้างโรงเอ</div>
+        </div> 
+        
+        <div class="tutoringjoin" id="tutoringjoin-3" style="display: none;">
+        <div class="join-button" id="join-3">
+          <div class="group">
+            <div class="button-text">เข้าร่วมกลุ่ม</div>
+            <img class="button-icon" alt="" src="../images/tutoringlist/tutoring-join.svg" />
+          </div>
+        </div>
+
+        <div class="member-button" id="member-3">
+          <div class="group">
+            <div class="button-text">สมาชิกกลุ่ม</div>
+            <img class="button-icon" alt="" src="../images/tutoringlist/tutoring-member.svg" />
+          </div>
+        </div>
+
+        <div class="close-button" id="close-3">
+              <div class="group">
+                <div class="button-text">ปิด</div>
+                <img class="button-icon1" alt="" src="../images/tutoringlist/tutoring-close.svg" />
+              </div>
+        </div>
+      </div>
+      
+      </div>
+
+    <script>
+      var display = 0;
+      var list1 = document.getElementById("list-1");
+      var tutoringjoin1 =document.getElementById("tutoringjoin-1");
+      var innerlist1 = document.getElementById("innerlist-1");
+
+      var list2 = document.getElementById("list-2");
+      var tutoringjoin2 =document.getElementById("tutoringjoin-2");
+      var innerlist2 = document.getElementById("innerlist-2");
+
+      var list3 = document.getElementById("list-3");
+      var tutoringjoin3 =document.getElementById("tutoringjoin-3");
+      var innerlist3 = document.getElementById("innerlist-3");
+      
+      function close_tutoringjoin(e){
+        tutoringjoin1.style.display = 'none';
+        tutoringjoin2.style.display = 'none';
+        tutoringjoin3.style.display = 'none';
+        document.querySelector('.padding')?.classList.remove('padding')
+      }
+
+      var close1 = document.getElementById("close-1");
+      close1.addEventListener("click", close_tutoringjoin);
+      var close2 = document.getElementById("close-2");
+      close2.addEventListener("click", close_tutoringjoin);
+      var close3 = document.getElementById("close-3");
+      close3.addEventListener("click", close_tutoringjoin);
+
+      var join1 = document.getElementById("join-1");
+      join1.addEventListener("click", function(e){
+        console.log("join group1 requested");
+        close_tutoringjoin();
+      });
+
+      var join2 = document.getElementById("join-2");
+      join2.addEventListener("click", function(e){
+        console.log("join group2 requested");
+        close_tutoringjoin();
+      });
+
+      var join3 = document.getElementById("join-3");
+      join3.addEventListener("click", function(e){
+        console.log("join group3 requested");
+        close_tutoringjoin();
+      });
+      
+      var member1 = document.getElementById("member-1")
+      member1.addEventListener("click", function (e) {
+        console.log("member group1 view requested")
+        close_tutoringjoin();
+      });
+
+      var member2 = document.getElementById("member-2")
+      member2.addEventListener("click", function (e) {
+        console.log("member group2 view requested")
+        close_tutoringjoin();
+      });
+
+      var member3 = document.getElementById("member-3")
+      member3.addEventListener("click", function (e) {
+        console.log("member group3 view requested")
+        close_tutoringjoin();
+      });
+
+      if (innerlist1) {
+        innerlist1.addEventListener("click", function (e) {
+          if (list1 != 'list-1.padding'){
+            close_tutoringjoin();
+            tutoringjoin1.style.display = 'block';
+            list1.classList.add('padding');
+          }
+        });
+      }
+
+      if (innerlist2) {
+        innerlist2.addEventListener("click", function (e) {
+          if (list2 != 'list-2.padding'){
+            close_tutoringjoin();
+            tutoringjoin2.style.display = 'block';
+            document.querySelector('.padding')?.classList.remove('padding')
+            list2.classList.add('padding');
+          }
+        });
+      }
+
+      if (innerlist3) {
+        innerlist3.addEventListener("click", function (e) {
+          if (list3 != 'list-3.padding'){
+            close_tutoringjoin();
+            tutoringjoin3.style.display = 'block';
+            document.querySelector('.padding')?.classList.remove('padding')
+            list3.classList.add('padding');
+          }
+        });
+      }
+
+    </script>
+
+  </body>
+</html>
