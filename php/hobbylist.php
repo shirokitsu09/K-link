805d3b26b7deb74b350363c2cee82357e59448ab
Not6629 <154392788+Not6629@users.noreply.github.com>
2024-01-30 18:53:09 +0700

    update new path

--- php/hobbylist.php
+++ php/hobbylist.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="initial-scale=1, width=device-width" />
     <link rel="stylesheet" href="../css/globalTEST.css" />
-    <link rel="stylesheet" href="../css/librarylist.css" />
+    <link rel="stylesheet" href="../css/grouplist.css" />
     <link
       rel="stylesheet"
       href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
@@ -13,41 +13,42 @@
   <body>
     <div class="list-frame">
       <div class="status-bar"></div>
+      <div class="tpopup" id="tpopup">
+        <div class="tpopup-drag" id="tpopupdrag"></div>
+        <div class="tpopup-option" id="tpopup-option-1">
+          <img class="tpopup-option-icon" src="../images/librarylist/tdotblock.svg">
+          <div class="tpopup-option-text">บล็อก</div> 
+        </div>
+        <div class="tpopup-option" id="tpopup-option-2">
+        <img class="tpopup-option-icon" src="../images/librarylist/report.svg">
+          <div class="tpopup-option-text">รายงาน</div> 
+        </div>
+      </div>
       <div class="list" id="list-1">
         <div class="list-inner">
         <div class="list-inner-head">
-        <img class="library-flag-banner" alt="" src="../images/grouplist/" />
-        <img class="Tdot-button" alt="" src="../images/threedot.svg" />
+        <img class="group-flag-banner" alt="" src="../images/grouplist/groupbanner.svg" />
+        <img class="tdot-button" id="tdot-1" alt="" src="../images/tutoringlist/threedot.svg" />
             <div class="tag-group">
-            <div class="tag">วิศวกรรมศาสตร์</div>
-            <div class="tag">AIJHONG</div>
+            <div class="tag" id="tag1-1">กีฬาและการออกกำลังกาย</div>
+            <div class="tag" id="tag1-2">ชิลๆ</div>
             </div>
             <div class="tag-group1">
-            <div class="tag">66665656 ENGINEERING STATISTICS</div>
+            <div class="tag"></div>
+            <div class="tag"></div>
             </div>
+        <b class="group-amount available">1/10</b>
         </div>
         <div class="list-inner-body" id="innerlist-1">
-        <b class="subject-name">Engineering Statistics</b>
-        <b class="subject-id">66665656</b>
-        <div class="subject-uploader">โดย : AIJHONG</div>
-        <img class="subject-file-picture" alt="" src="../images/librarylist/engstat.svg" />
-        <b class="subject-faculty">คณะ วิศวกรรมศาสตร์</b>
-        <div class="subject-description">รายละเอียด : มีสรุปเรื่อง Gaussian</div>
+        <b class="group-name">หาเพื่อนไปวิ่งกันเถอะ232...</b>
+        <div class="leader">AIJHONG</div>
+        <img class="group-profile-picture" alt="" src="../images/grouplist/group-profilepic1.svg" />
+        <div class="group-date">วัน : 15 มิ.ย. 2567</div>
+        <div class="group-time">เวลา : 17:00 - 19:00 น.</div>
+        <div class="group-location">สถานที่ : สนามกีฬา</div>
+        <div class="group-description">รายละเอียด : ผู้เล่นจะได้รับบทบาทเป็น โกะโจ เพื่อช่วยเหล่าผู้เล่นให้ได้มากที่สุด ขณะเดียวกันผู้เล่นคนอื่น ๆ </div>
         </div>
-        <div class="list-inner-bottom">
-        <div class="like-button" id="like-1">
-              <img src="../images/librarylist/like-button.svg">
-              <b class="like-amount">24</b>
-            </div>
-            <div class="download-button" id="download-1">
-              <img src="../images/librarylist/download-button.svg">
-            </div>
-            <div class="share-button" id="share-1">
-              <img src="../images/librarylist/share-button.svg">
-            </div>
-            </div>
-        
-        </div>
+      </div>
         
         <div class="tutoringjoin" id="tutoringjoin-1" style="display: none;">
         <div class="join-button" id="join-1" value="join-1">
@@ -76,36 +77,27 @@
       <div class="list" id="list-2">
         <div class="list-inner">
         <div class="list-inner-head">
-        <img class="library-flag-banner" alt="" src="../images/grouplist/groupbanner.svg" />
-        <img class="Tdot-button" alt="" src="../images/threedot.svg" />
+        <img class="group-flag-banner" alt="" src="../images/grouplist/groupbanner.svg" />
+        <img class="tdot-button" id="tdot-2" alt="" src="../images/tutoringlist/threedot.svg" />
                 <div class="tag-group">
-                <div class="tag">วิศวกรรมศาสตร์</div>
-                <div class="tag">AIJHONG</div>
+                <div class="tag" id="tag2-1">อาหารการกิน</div>
+                <div class="tag" id="tag2-2">ชาบูปิ้งย่าง</div>
+                <div class="tag" id="tag2-3">ซอยเกกี</div>
                 </div>
                 <div class="tag-group1">
-                <div class="tag">66665656 CALCULUS2</div>
+                <div class="tag"></div>
                 </div>
-            </div>
-            <div class="list-inner-body" id="innerlist-2">
-            <b class="subject-name">Calculus 2</b>
-            <b class="subject-id">66665656</b>
-            <img class="subject-file-picture" alt="" src="../images/librarylist/engstat.svg" />
-            <div class="subject-uploader">ราชาหมูกะทะผู้ยิ่งใหญ่</div>
-            <b class="subject-faculty">คณะ วิศวกรรมศาสตร์</b>
-            <div class="subject-description">รายละเอียด : มีสรุปเรื่อง Vector , มีแบบฝึกหัด</div>
-            </div>
-            <div class="list-inner-bottom">
-            <div class="like-button">
-              <img src="../images/librarylist/like-button.svg">
-              <b class="like-amount">15</b>
-            </div>
-            <div class="download-button">
-              <img src="../images/librarylist/download-button.svg">
-            </div>
-            <div class="share-button">
-              <img src="../images/librarylist/share-button.svg">
-            </div>
-            </div>
+            <b class="group-amount full">4/4</b>
+        </div>
+        <div class="list-inner-body" id="innerlist-2">
+            <b class="group-name">หมูกะทะหารสี่</b>
+            <div class="leader">ราชาหมูกะทะผู้ยิ่งใหญ่</div>
+            <img class="group-profile-picture" alt="" src="../images/grouplist/group-profilepic2.svg" />
+            <div class="group-description">รายละเอียด : มากินหมูกะทะกันหาร 4 คน</div>
+            <div class="group-date">วัน : 25 ก.ค. 2589</div>
+            <div class="group-time">เวลา : 16:00 - 19:00 น.</div>
+            <div class="group-location">สถานที่ : หมูกะทะเกกี4</div>
+        </div>
         </div>
         
         <div class="tutoringjoin" id="tutoringjoin-2" style="display: none;">
@@ -135,37 +127,25 @@
       <div class="list" id="list-3">
         <div class="list-inner">
         <div class="list-inner-head">
-        <img class="library-flag-banner" alt="" src="../images/grouplist/groupbanner.svg" />
-        <img class="Tdot-button" alt="" src="../images/threedot.svg" />
+        <img class="group-flag-banner" alt="" src="../images/grouplist/groupbanner.svg" />
+        <img class="tdot-button" id="tdot-3" alt="" src="../images/tutoringlist/threedot.svg" />
             <div class="tag-group">
             <div class="tag">วิศวกรรมศาสตร์</div>
-            <div class="tag">พี่บ็อบ</div>
+            <div class="tag">Calculus</div>
+            <div class="tag"></div>
             </div>
-            <div class="tag-group1">
-            <div class="tag">66665656 ELECTRONIC CIRCUITS</div>
+            <b class="group-amount available">10/ไม่จำกัด</b>
             </div>
-          </div>
-        <div class="list-inner-body" id="innerlist-3">
-            <b class="subject-name">Electronic circuits</b>
-            <b class="subject-id">66665656</b>
-            <img class="subject-file-picture" alt="" src="../images/librarylist/scielec.svg" />
-            <div class="subject-uploader">เหน่ง iot</div>
-            <b class="subject-faculty">คณะ วิทยาศาสตร์</b>
-            <div class="subject-description">รายละเอียด : มีสรุปเรื่อง Transistor Amplifier </div>
+            <div class="list-inner-body" id="innerlist-3">
+            <b class="group-name">เชียร์บาสภาคคอมรวมตัว</b>
+            <div class="leader">เหน่ง iot</div>
+            <img class="group-profile-picture" alt="" src="../images/grouplist/group-profilepic3.svg" />
+            <div class="group-description">รายละเอียด : เพื่อช่วยเหล่าผู้เล่นให้ได้มากที่สุด ขณะเดียวกันผู้เล่นคนอื่น ๆ </div>
+            <div class="group-date">วัน : 9 ธ.ค. 2565</div>
+            <div class="group-time">เวลา : 17:00 - 21:00 น.</div>
+            <div class="group-location">สถานที่ : สนามบาสข้างโรงเอ</div>
             </div>
-            <div class="list-inner-bottom">
-            <div class="like-button">
-              <img src="../images/librarylist/like-button.svg">
-              <b class="like-amount">6</b>
-            </div>
-            <div class="download-button">
-              <img src="../images/librarylist/download-button.svg">
-            </div>
-            <div class="share-button">
-              <img src="../images/librarylist/share-button.svg">
-            </div>
-            </div>
-        </div>
+        </div> 
         
         <div class="tutoringjoin" id="tutoringjoin-3" style="display: none;">
         <div class="join-button" id="join-3">
@@ -197,6 +177,7 @@
       var list1 = document.getElementById("list-1");
       var tutoringjoin1 =document.getElementById("tutoringjoin-1");
       var innerlist1 = document.getElementById("innerlist-1");
+      var tpopup = document.getElementById("tpopup"); // three dot pop up id
 
       var list2 = document.getElementById("list-2");
       var tutoringjoin2 =document.getElementById("tutoringjoin-2");
@@ -212,6 +193,15 @@
         tutoringjoin3.style.display = 'none';
         document.querySelector('.padding')?.classList.remove('padding')
       }
+      var tag_1= document.getElementById("tag1-1");
+      tag_1.addEventListener("click", function(){
+        location.href = "";
+      });
+
+      var tag_2 = document.getElementById("tag1-2");
+      tag_2.addEventListener("click", function(){
+        location.href = "";
+      });
 
       var close1 = document.getElementById("close-1");
       close1.addEventListener("click", close_tutoringjoin);
@@ -256,6 +246,50 @@
         close_tutoringjoin();
       });
 
+      function tpopup_open(e){
+        tpopup.classList.add("on");
+      }
+
+      function tpopup_close(e){
+        tpopup.classList.remove("on");
+      }
+
+      tdot1 = document.getElementById("tdot-1")
+      tdot1.addEventListener("click",function (e) {
+        close_tutoringjoin();
+        tpopup_open();
+      });
+
+      tdot2 = document.getElementById("tdot-2")
+      tdot2.addEventListener("click",function (e) {
+        close_tutoringjoin();
+        tpopup_open();
+      });
+
+      tdot3 = document.getElementById("tdot-3")
+      tdot3.addEventListener("click",function (e) {
+        close_tutoringjoin();
+        tpopup_open();
+      });
+
+      var tpopupdrag = document.getElementById("tpopupdrag");
+      tpopupdrag.addEventListener("click",function(e){
+        tpopup_close();
+      });
+
+      var tpopupoption1 = document.getElementById("tpopup-option-1");
+      tpopupoption1.addEventListener("click", function(e){
+        console.log("blocked request");
+        tpopup_close();
+      });
+
+      var tpopupoption2 =document.getElementById("tpopup-option-2");
+      tpopupoption2.addEventListener("click", function(e){
+        console.log("reported request")
+        tpopup_close();
+      });
+
+// 
       if (innerlist1) {
         innerlist1.addEventListener("click", function (e) {
           if (list1 != 'list-1.padding'){
