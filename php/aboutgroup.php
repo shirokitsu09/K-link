35c297e230a2ac9971f23d53da7759d3c3c3ece1
Not6629 <154392788+Not6629@users.noreply.github.com>
2024-01-30 14:32:44 +0700

    upload code

---
+++ php/aboutgroup.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="initial-scale=1, width=device-width" />
+
+    <link rel="stylesheet" href="../css/global.css" />
+    <link rel="stylesheet" href="../css/aboutgroup.css" />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+    />
+  </head>
+  <body>
+    <div class="hobby">
+      <div class="about-group-header">
+      <div class="group-child"></div>
+        <b class="header-text">ABOUT GROUP</b>
+        <img class="back-button" alt="" src="../images/backbutton.svg" />
+
+        <img class="noti-button-icon" alt="" src="../images/noti.svg" />
+      </div>
+      
+
+      <div class="about-group-body">
+        <div class="body-background">
+        <img class="tdot-button" alt="" src="../images/threedot.svg" />
+          <b class="group-name">ชื่อกลุ่ม</b>
+            <img class="group-picture" alt="" src="../images/" />
+
+          <div class="group-date">
+            <div class="group-date-title">วันทำกิจกกรม</div>
+            <div class="group-date-container">
+              <div class="ellipse-date">
+                <div class="date">จ</div>
+              </div>
+              <div class="ellipse-date">
+                <div class="date">อ</div>
+              </div>
+              <div class="ellipse-date">
+                <div class="date">พ</div>
+              </div>
+              <div class="ellipse-date">
+                <div class="date">พฤ</div>
+              </div>
+              <div class="ellipse-date">
+                <div class="date">ศ</div>
+              </div>
+              <div class="ellipse-date">
+                <div class="date">ส</div>
+              </div>
+              <div class="ellipse-date">
+                <div class="date">อา</div>
+              </div>
+            </div>
+          </div>
+          
+          <div class="group-location">
+          <div class="group-location-title">สถานที่</div>
+            <div class="group-location-container">
+              <div class="group-location-text">โดมเห็ดรูม A (ข้างโรงเอ วิศวะ)</div>
+            </div>
+            
+            <div class="group-member">
+              <div class="group-member-text">สมาชิก :</div>
+              <div class="rectangle-parent2">
+                <div class="group-child6"></div>
+                <img class="vector-icon1" alt="" src="../images/aboutgroup/magnifyglass.svg" />
+              </div>
+              <div class="div10">
+                <div class="">25</div><div class="">/ไม่จำกัด</div>
+              </div>
+            </div>
+            <div class="group-time">
+            <div class="group-time-title">เวลา :</div>
+            <div class="group-time-time">20.00</div>
+            </div>
+          </div>
+          
+          <div class="group-parent1">
+            <div class="rectangle-parent4">
+              <div class="group-child8"></div>
+              <div class="div13">วิ่งเก็บเห็ด</div>
+            </div>
+            <div class="div14">รายละเอียด</div>
+          </div>
+          <div class="rectangle-parent5">
+            <div class="group-child9">
+            <div class="div17">วิ่ง</div>
+            <div class="div17">กีฬา</div>
+            <div class="div17">ช่วงเย็น</div>
+            <div class="div17">สนามกีฬา</div>
+            <div class="div17">ออกกำลังกาย</div>
+            </div>
+            <div class="frame-container">
+              <div class="group-wrapper">
+                <div class="rectangle-parent6">
+                  
+                </div>
+              </div>
+            </div>
+            <div class="group-frame">
+                  <div class="group-wrapper1">
+                    <div class="group-child11"></div>
+                    
+              </div>
+            </div>
+            <div class="tag">Tag</div>
+                <div class="rectangle-parent8">
+
+
+            </div>
+          </div>
+          <div class="rectangle-parent10">
+          <div class="group-child14"></div>
+          <div class="div20">ชวนเพื่อนเข้ากลุ่ม</div>
+          <img class="vector-icon2" alt="" src="../images/aboutgroup/addfriendtogroup.svg" />
+        </div>
+        </div>
+      </div>
+    </div>
+
+    <script>
+      var groupContainer11 = document.getElementById("groupContainer11");
+      if (groupContainer11) {
+        groupContainer11.addEventListener("click", function (e) {
+          // Please sync "tutoring  group 12" to the project
+        });
+      }
+      
+      var groupContainer22 = document.getElementById("groupContainer22");
+      if (groupContainer22) {
+        groupContainer22.addEventListener("click", function (e) {
+          // Please sync "user edit" to the project
+        });
+      }
+      
+      var frameIcon = document.getElementById("frameIcon");
+      if (frameIcon) {
+        frameIcon.addEventListener("click", function (e) {
+          // Please sync "ThreeDotsOption" to the project
+        });
+      }
+      </script>
+  </body>
+</html>
