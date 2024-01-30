9141b1ed8500b0c4a9e96dc4c8d97dc0b6908681
Volthnut <154393992+Volthnut@users.noreply.github.com>
2024-01-30 19:35:44 +0700

    MrJ

---
+++ php/hobbyHome.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hobby-Home</title>
+
+    <link rel="stylesheet" href="../css/hobby-home.css" />
+    <link rel="stylesheet" href="../css/globalTEST.css" />
+    <link rel="stylesheet" href="../css/HEADER.css" />
+    <link rel="stylesheet" href="../css/FOOTER.css" />
+    <link rel="stylesheet" href="../css/creategroup.css" />
+
+</head>
+<body>
+    <div class="background">
+        <?php include 'header.php';?>
+        <?php include 'footer.php';?>
+        <?php include 'hobbyList.php';?>
+    </div>
+
+</body>
+<!------------------------------ script ------------------------------>
+    <script>
+        function changeText() {
+          let text = document.querySelector('.name-header');
+          let list = document.querySelector('.list');
+          let indicator = document.getElementById('indicator');
+            text.textContent = 'Hobby';
+            list.classList.remove('active');
+            indicator.classList.add('hidden');
+          }
+        changeText();
+    </script>
+<!------------------------------ script ------------------------------>
+
+</html>
--- php/setting.php
+++ php/setting.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="initial-scale=1, width=device-width" />
 
-    <link rel="stylesheet" href="./css/global2.css" />
-    <link rel="stylesheet" href="./css/setting.css" />
+    <link rel="stylesheet" href="../css/globalTEST.css" />
+    <link rel="stylesheet" href="../css/setting.css" />
     <link
       rel="stylesheet"
       href="https://fonts.googleapis.com/css2?family=Roboto:wght@400&display=swap"
@@ -16,102 +16,98 @@
     />
   </head>
   <body>
-    <div class="setting">
-      <img
-        class="gesturethreebuttons-icon"
-        alt=""
-        src="./public/gesturethreebuttons.svg"
-      />
+    <div class="background">
 
-      <!-- <div class="status-bar">
-        <img class="container-icon" alt="" src="./public/container.svg" />
+      <div class="setting-head">
+        <img class="setting-item" alt="" src="../images/backbutton.svg" id="backbutton"/>
+        <div class="header-text">SETTING</div>
+        <img class="noti-button-icon" alt="" src="../images/noti.svg" id="notiButtonIcon"/>
+      </div>
 
-        <div class="status-bar-contents">
-          <img class="container-icon" alt="" src="./public/bounds.svg" />
-
-          <img class="wifi-icon" alt="" src="./public/wifi.svg" />
-
-          <img class="reception-icon" alt="" src="./public/reception.svg" />
-
-          <img class="battery-icon" alt="" src="./public/battery.svg" />
-
-          <div class="time">
-            <div class="div">12:30</div>
-          </div>
+      <div class="setting-inner">
+        <div class="option-parent" id="setting-option-1">
+          <div class="option-text">about account</div>
+          <img class="option-icon" alt="" src="../images/setting/profile-icon.svg"/>
         </div>
-      </div> -->
-      <div class="setting-child"></div>
-      <div class="account">ACCOUNT</div>
-      <img class="setting-item" alt="" src="./public/frame-420.svg" />
 
-      <div class="setting-inner"></div>
-      <div class="vector-parent">
-        <img class="vector-icon" alt="" src="./public/vector.svg" />
+        <div class="option-parent" id="setting-option-2">
+          <img class="option-icon" alt="" src="../images/setting/bookmark.svg" />
+          <div class="option-text">saved</div>
+        </div>
 
-        <div class="setting1">setting</div>
-      </div>
-      <div class="language-parent">
-        <div class="language">language</div>
-        <img class="icon-global" alt="" src="./public/-icon-global.svg" />
-      </div>
-      <div class="about-app-parent">
-        <div class="about-app">about app</div>
-        <img class="group-child" alt="" src="./public/group-202.svg" />
-      </div>
-      <div class="about-account-parent" id="groupContainer3">
-        <div class="about-account">about account</div>
-        <img
-          class="icon-profile-circle"
-          alt=""
-          src="./public/-icon-profile-circle.svg"
-        />
-      </div>
-      <div class="vector-group" id="groupContainer4">
-        <img class="vector-icon1" alt="" src="./public/vector1.svg" />
+        <div class="option-parent" id="setting-option-3">
+            <img class="option-icon" alt="" src="../images/setting/notification-icon.svg" />
+            <div class="option-text">notification</div>
+            <div1 class="option-mode" id="notificationMode"></div1>
+        </div>
 
-        <div class="edit-my-post">edit my post</div>
-      </div>
-      <div class="status-bar">
-        <img class="container-icon" alt="" src="./public/container1.svg" />
-
-        <div class="status-bar-contents">
-          <img class="container-icon" alt="" src="./public/bounds1.svg" />
-
-          <img class="wifi-icon" alt="" src="./public/wifi1.svg" />
-
-          <img class="reception-icon" alt="" src="./public/reception1.svg" />
-
-          <img class="battery-icon" alt="" src="./public/battery1.svg" />
-
-          <div class="time">
-            <div class="div">12:30</div>
-          </div>
+        <div class="option-parent" id="setting-option-4">
+          <img class="option-icon" alt="" src="../images/setting/about-app-icon.svg" />
+          <div class="option-text">about app</div>
         </div>
       </div>
-      <img class="noti-button-icon" alt="" src="./public/noti-button.svg" />
+      
+      
 
-      <div class="notification">
-        <div class="parent">
-          <div class="div2">notification on</div>
-          <img class="instance-child" alt="" src="./public/frame-215.svg" />
-        </div>
-      </div>
     </div>
 
+
     <script>
-      var groupContainer3 = document.getElementById("groupContainer3");
-      if (groupContainer3) {
-        groupContainer3.addEventListener("click", function (e) {
-          // Please sync "About my account" to the project
+      var backButton = document.getElementById("backbutton");
+      if (backButton) {
+        backButton.addEventListener("click", function (e) {
+          window.location.href = "";
         });
       }
-      
-      var groupContainer4 = document.getElementById("groupContainer4");
-      if (groupContainer4) {
-        groupContainer4.addEventListener("click", function (e) {
-          // Please sync "my post" to the project
+  
+      var notiButtonIcon = document.getElementById("notiButtonIcon");
+      if (notiButtonIcon) {
+        notiButtonIcon.addEventListener("click", function (e) {
+          window.location.href = "";
         });
       }
-      </script>
+  
+      var aboutAccountParent = document.getElementById("aboutAccountParent");
+      if (aboutAccountParent) {
+        aboutAccountParent.addEventListener("click", function (e) {
+          window.location.href = "";
+        });
+      }
+  
+      var option2 = document.getElementById("option2");
+      if (option2) {
+        option2.addEventListener("click", function (e) {
+          window.location.href = "";
+        });
+      }
+  
+      var languageParent = document.getElementById("languageParent");
+      if (languageParent) {
+        languageParent.addEventListener("click", function (e) {
+          window.location.href = "";
+        });
+      }
+
+      var notificationMode = document.getElementById("notificationMode");
+      if (notificationMode) {
+        notificationMode.addEventListener("click", function (e) {
+          {document.getElementById("notificationMode").innerHTML = "off";}
+      });
+
+      var settingParent = document.getElementById("settingParent");
+      if (settingParent) {
+        settingParent.addEventListener("click", function (e) {
+          window.location.href = "";
+        });
+      }
+
+      var aboutAppParent = document.getElementById("aboutAppParent");
+      if (aboutAppParent) {
+        aboutAppParent.addEventListener("click", function (e) {
+          window.location.href = "";
+        });
+      }
+    }
+    </script>
   </body>
 </html>
