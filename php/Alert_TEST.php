5dd17514aa7cf8732aa2d49aa4391ac6b8405c55
“KlausJager10110” <pacharapolpacharapol2547@gmail.com>
2024-02-03 21:14:38 +0700

    Alert update1

---
+++ php/Alert_TEST.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/Alert-Test.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+    <script src="../js/alert-Test.js" defer></script>
+    <title>Alert</title>
+</head>
+<body>
+    <!-- <ul class="notifications">
+        <li class="toast success">
+            <div class="column">
+                <i class="fa-solid fa-circle-check"></i>
+                <span>Success : 1111111.</span>
+            </div>
+            <i class="fa-solid fa-xmark"></i>
+        </li>
+        <li class="toast error">
+            <div class="column">
+                <i class="fa-solid fa-circle-xmark"></i>
+                <span>Error : 222222.</span>
+            </div>
+            <i class="fa-solid fa-xmark"></i>
+        </li>
+        <li class="toast warning">
+            <div class="column">
+                <i class="fa-solid fa-triangle-exclamation"></i>
+                <span>Warning : 333333.</span>
+            </div>
+            <i class="fa-solid fa-xmark"></i>
+        </li>
+        <li class="toast info">
+            <div class="column">
+                <i class="fa-solid fa-circle-info"></i>
+                <span>Info : 4444444444.</span>
+            </div>
+            <i class="fa-solid fa-xmark"></i>
+        </li>
+    </ul> -->
+    <ul class="notifications"></ul>
+    <div class="buttons">
+        <button class="btn" id="success">Success</button>
+        <button class="btn" id="error">Error</button>
+        <button class="btn" id="warning">Warning</button>
+        <button class="btn" id="info">Info</button>
+    </div>
+</body>
+<script>
+    
+</script>
+</html>
