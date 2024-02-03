a08025ef0f1973b513f9d64d06280ad41d0a69b6
Not6629 <154392788+Not6629@users.noreply.github.com>
2024-02-03 18:46:50 +0700

    Add member and edit hobbyaboutgroup

---
+++ php/member.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="initial-scale=1, width=device-width" />
+    <link rel="stylesheet" href="../css/globalTEST.css" />
+    <link rel="stylesheet" href="../css/member.css" />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+    />
+  </head>
+  <body>
+    <div class="list-frame">
+      <div class="status-bar"></div>
+      <div class="about-group-header">
+        <b class="header-text">สมาชิก</b>
+        <img class="back-button" alt="" src="../images/backbutton.svg" />
+      </div>
+        <div class="list-inner">
+        <div class="list-inner-head">
+            <div class="title-group">
+            <b class="title" id="title">แคล2</b>
+            </div>
+        <b class="group-amount available">1/10</b>
+        </div>
+        <ol class="list-inner-body">
+        <li class="list">
+        <div class="leader">AIJHONG</div>
+        <img class="leader-icon" alt="" src="../images/member/leader.svg" />
+        <img class="info-icon" alt="" src="../images/member/moreInfo.svg" />
+</li>
+        <li class="list">
+        <div class="member">AiJane</div>
+        <img class="me-icon" alt="" src="../images/member/me.svg" />
+</li>
+</div>
+</ol>
+      </div>
+
+  <script type="text/javascript">
+
+  </script>
+    </body>
+</html>
