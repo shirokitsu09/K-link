f1b5f7ded904bcb56c543001d0091a3ea9b9e724
“KlausJager10110” <pacharapolpacharapol2547@gmail.com>
2024-02-04 18:31:37 +0700

    edit

---
+++ php/HorizontalSlideBar.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Horizontal Slid Bar</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+    <link rel="stylesheet" href="../css/HoriSlideBar.css">
+    <script src="../js/HorizontalSlideBar.js" defer></script>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="icon"><i id="left" class="fa-solid fa-angle-left"></i></div>
+        <ul class="tabs-box">
+            <li class="tab">Coding</li>
+            <li class="tab active">JS</li>
+            <li class="tab">CSS</li>
+            <li class="tab">PHP</li>
+            <li class="tab">Py</li>
+            <li class="tab">1asdsdawda</li>
+            <li class="tab">2dawdasda</li>
+            <li class="tab">3atwttt</li>
+            <li class="tab">sadasd4</li>
+            <li class="tab">5sdasd</li>
+            <li class="tab">6sadasdsdasd</li>
+            <li class="tab">7asdasdwsd</li>
+            <li class="tab">7taw22</li>
+            <li class="tab">7wdatata</li>
+            <li class="tab">7dawtawt</li>
+        </ul>
+        <div class="icon"><i id="right" class="fa-solid fa-angle-right"></i></div>
+    </div>
+</body>
+</html>
