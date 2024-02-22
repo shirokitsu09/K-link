e71aa07732b2fe760f66e309519c93d69a26d318
shirokitsu09 <150586092+shirokitsu09@users.noreply.github.com>
2024-02-22 20:45:35 +0700

    Delete php directory

--- php/mypost.php
+++
@@ -1,6 +0,0 @@
-<link rel="stylesheet" href="../css/MYGROUP.css">
-<link rel="stylesheet" href="../css/globalTEST.css">
-
-
-<?php include '../php/HorizontalSlideBar.php'; ?>
-<?php include '../php/tutoringlist.php';?>
